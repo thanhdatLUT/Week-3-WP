6b58e12f0ad2a14645eecb5ca6d47de295f8e510
thanhdatLUT <thanhdat231027@gmail.com>
2026-06-22 16:45:09 +0700

    Update project

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ async function loadData() {
 
     const employmentQuery = await fetch("employment_query.json").then(res => res.json());
 
-    const populationResponse = await fetch("https://pxdata.stat.fi:443/PxWeb/api/v1/fi/StatFin/vaerak/statfin_vaerak_pxt_11ra.px", {
+    const populationResponse = await fetch("https://pxdata.stat.fi/PxWeb/api/v1/fi/StatFin/11ra.px", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
@@ -14,7 +14,7 @@ async function loadData() {
     const populationData = await populationResponse.json();
     console.log(populationData);
 
-    const employmentResponse = await fetch("https://pxdata.stat.fi:443/PxWeb/api/v1/fi/StatFin/tyokay/statfin_tyokay_pxt_115b.px", {
+    const employmentResponse = await fetch("https://pxdata.stat.fi/PxWeb/api/v1/fi/StatFin/tyokay/115b.px", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
