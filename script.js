f27646a5bb5048b15264acb23d01b50880de4979
thanhdatLUT <thanhdat231027@gmail.com>
2026-06-22 16:14:32 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,62 @@
+async function loadData() {
+    const populationQuery = await fetch("population_query.json").then(res => res.json());
+
+    const employmentQuery = await fetch("employment_query.json").then(res => res.json());
+
+    const populationResponse = await fetch("https://pxdata.stat.fi/PxWeb/api/v1/fi/StatFin/11ra.px", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(populationQuery)
+    });
+    
+    const populationData = await populationResponse.json();
+
+    const employmentResponse = await fetch("https://pxdata.stat.fi/PxWeb/api/v1/fi/StatFin/tyokay/115b.px", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(employmentQuery)
+    });
+
+    const employmentData = await employmentResponse.json();
+
+    const municipalities =
+    Object.values(populationData.dataset.dimension.alue_23_20260101.category.label);
+
+    const populationValues = populationData.dataset.value;
+
+    const employmentValues = employmentData.dataset.value;
+
+    const tbody = document.querySelector("tbody");
+
+    municipalities.forEach((municipality, index) => {
+
+        const population = populationValues[index];
+        const employment = employmentValues[index];
+
+        const percentage = (employment / population * 100).toFixed(2);
+
+        const row = document.createElement("tr");
+
+        if (percentage >= 45) {
+            row.style.backgroundColor = #abffbd;
+        } else if (percentage < 25) {
+            row.style.backgroundColor = #ff9e9e;
+        }
+        
+        row.innerHTML = `
+            <td>${municipality}</td>
+            <td>${population}</td>
+            <td>${employment}</td>
+            <td>${percentage}%</td>
+        `;
+
+        tbody.appendChild(row);
+    });
+
+}
+
+loadData();
